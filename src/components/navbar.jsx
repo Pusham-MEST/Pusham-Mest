c8a2a93c8ffc4f56d2e3676bc5fd25dc07dfdfe2
CharlesAduSark <charlesadusarkodie@gmail.com>
2024-07-29 12:54:06 +0000

    setting up the alert page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,90 +1,42 @@
-import React, { useState } from "react";
-import { logo } from "../assets";
+import React from 'react'
+import { logo } from '../assets'
 
-const Navbar = () => {
-  const [open, setOpen] = useState(false);
-
+const navbar = () => {
   return (
-    <header className={`flex w-full items-center bg-white dark:bg-dark`}>
-      <div className="container">
-        <div className="relative -mx-4 flex items-center justify-between">
-          <div className="w-60 max-w-full px-4">
-            <a href="/#" className="block w-full py-5">
-              <img
-                src={logo}
-                alt="logo"
-                className="dark:hidden"
-              />
-              <img
-                src={logo}
-                alt="logo"
-                className="hidden dark:block"
-              />
-            </a>
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <img className="btn btn-ghost text-xl" src={logo} alt="Logo" />
+        </div>
+        <div className="flex-none gap-2">
+          <div className="form-control">
+            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
           </div>
-          <div className="flex w-full items-center justify-between px-4">
-            <div>
-              <button
-                onClick={() => setOpen(!open)}
-                id="navbarToggler"
-                className={` ${
-                  open && "navbarTogglerActive"
-                } absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
-              >
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-              </button>
-              <nav
-                // :className="!navbarOpen && 'hidden' "
-                id="navbarCollapse"
-                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none lg:dark:bg-transparent ${
-                  !open && "hidden"
-                } `}
-              >
-                <ul className="block lg:flex">
-                  <ListItem NavLink="/#">Menu</ListItem>
-                  {/* <ListItem NavLink="/#">Payment</ListItem>
-                  <ListItem NavLink="/#">About</ListItem>
-                  <ListItem NavLink="/#">Blog</ListItem> */}
-                </ul>
-              </nav>
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Logo"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
             </div>
-            <div className="hidden justify-end pr-16 sm:flex lg:pr-0">
-              <a
-                href="/#"
-                className="px-7 py-3 text-base font-medium text-dark hover:text-primary dark:text-white"
-              >
-                Log In
-              </a>
-
-              <a
-                href="/#"
-                className="rounded-md bg-primary px-7 py-3 text-base font-medium text-white hover:bg-primary/90"
-              >
-                Sign Up
-              </a>
-            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              <li>
+                <a className="justify-between">
+                  Sign Up
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li><a>Login</a></li>
+              <li><a>Logout</a></li>
+            </ul>
           </div>
         </div>
       </div>
-    </header>
-  );
-};
+    </div>
+  )
+}
 
-export default Navbar;
-
-const ListItem = ({ children, NavLink }) => {
-  return (
-    <>
-      <li>
-        <a
-          href={NavLink}
-          className="flex py-2 text-base font-medium text-body-color hover:text-dark dark:text-dark-6 dark:hover:text-white lg:ml-12 lg:inline-flex"
-        >
-          {children}
-        </a>
-      </li>
-    </>
-  );
-};
+export default navbar
